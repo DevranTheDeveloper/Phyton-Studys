a007918ce9d55440c9fd614d99754bd8ab64ec67
Devran Sever <137220822+DevranTheDeveloper@users.noreply.github.com>
2023-08-03 18:28:42 +0300

    deneme

---
+++ Day-2/Day-2.py
@@ -0,0 +1,8 @@
+# f-string bütün tipleri stringe dönüştürmeye yarar çift tırnaktan hemen önce konur
+
+x = "Devran"
+y = 18
+z = 1.9
+t = True
+
+print(f"Merhaba {x}, yaşınız {y}, boyunuz {z}m, mezuniyet durumunuz: {t}")
