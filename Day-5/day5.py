2a20d2cb2ea512395f7ace13c19dd5aa02bda4bd
Devran Sever <137220822+DevranTheDeveloper@users.noreply.github.com>
2023-08-07 09:41:54 +0300

    day5

---
+++ Day-5/day5.py
@@ -0,0 +1,20 @@
+fruits = ["Apple", "Peach", "Pear"]
+
+for fruit in fruits:
+    print(fruit)
+    print(fruit + " Pie")
+print(fruits)
+
+#  sum() fonksiyonu bütün elemanları toplar 
+#  max() en büyüğü bulur 
+#  max() en küçüğü bulur 
+
+
+for number in range(1, 11, 3): # 3. kısım artış mıktarı oluyor
+    print(number)
+    
+total = 0
+for number in range(1, 101):
+    total += number
+    
+print(total)
